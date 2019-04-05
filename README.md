e9a36bce8efefcfac239b7e8ac73a175d8df56af
Timothée Malard <t.malard@valdun.net>
2019-04-05 16:00:24 +0200

    Add config

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # eslint-config-creanetis
+
+The Creanetis config for ESLint
+
+## Installation
+```
+yarn add --dev eslint-config-creanetis eslint eslint-plugin-import eslint-config-airbnb
+```
+
+## Set up a .eslintrc in your project
+```
+{
+  "extends": "creanetis"
+}
+
+```
+
+## Add it to your project's package.json
+```
+{
+  "scripts": {
+    "lint": "eslint .",
+  }
+}
+```
+
+## Run the linter
+```
+yarn run lint
+```
---
+++ index.js
@@ -0,0 +1,35 @@
+
+'use strict';
+
+module.exports = {
+  extends: [
+    'airbnb/base'
+  ],
+  rules: {
+    'arrow-parens': ["error", "as-needed"],
+    'comma-dangle': [2, 'always-multiline'],
+    'no-use-before-define': [2, 'nofunc'],
+    'no-param-reassign': 2,
+    'no-underscore-dangle': [0],
+    'newline-per-chained-call': [2],
+    'object-curly-newline': ["error", "always"],
+    'require-await': 2
+    strict: [2, 'global'],
+    'vars-on-top': 2,
+  },
+  globals: {
+    describe: true,
+    it: true,
+    before: true,
+    after: true,
+    beforeEach: true,
+    afterEach: true
+  },
+  env: {
+    node: true,
+    mocha: true
+  },
+  parserOptions: {
+    ecmaVersion: 10,
+  }
+};
